f1edbf5835332966de0d6a28bd22aec85a520433
noahzissman <noahzissman@icloud.com>
2025-04-10 15:19:45 -0500

    Second and Final Code using GPIOZero to make the LEDS work.
    PI didn't support the first type of code, so I re-wrote it using the same one in the Quiz2 Review.

--- QUIZ2.py
+++ QUIZ2.py
@@ -1,36 +1,21 @@
-import RPi.GPIO as GPIO
-import time
+#Noah Zissman
+#TEC 284
+from gpiozero import LED, Button   
+import time #delay timer
+from signal import pause
 
-RED_PIN = 17
-GREEN_PIN=27
-BLUE_PIN = 22
+red_led = LED(17) #Pin assignment
+green_led = LED(27)
+blue_led = LED(22)
 
-BTN_RED = 5
-BTN_GREEN = 6
-BTN_BLUE = 13
+btn_red = Button(5) #Button Assignment 
+btn_green = Button(6)
+btn_blue = Button(13)
 
 def setup():
-    GPIO.setmode(GPIO.BCM)
-    
-    GPIO.setup(RED_PIN, GPIO.OUT)
-    GPIO.setup(GREEN_PIN, GPIO.OUT)
-    GPIO.setup(BLUE_PIN, GPIO.OUT)
-    
-    GPIO.setup(BTN_RED, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(BTN_GREEN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(BTN_BLUE, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    
-def set_color(r, g, b):
-    
-    GPIO.output(RED_PIN, r)
-    GPIO.output(GREEN_PIN, g)
-    GPIO.output(BLUE_PIN, b)
-
-def read_buttons():
-    red = GPIO.input(BTN_RED)
-    green = GPIO.input(BTN_GREEN)
-    blue = GPIO.input(BTN_BLUE)
-    return red, green, blue
+    red_led.off()
+    green_led.off()
+    blue_led.off()
 
 def main():
     setup()
@@ -38,16 +23,23 @@ def main():
     
     try:
         while True:
-            r, g, b = read_buttons()
-            set_color(r, g, b)
+            if btn_red.is_pressed:
+                red_led.on()
+            else:
+                red_led.off()
+            
+            if btn_green.is_pressed:
+                green_led.on()
+            else:
+                green_led.off()
+            
+            if btn_blue.is_pressed:
+                blue_led.on()
+            else:
+                blue_led.off()
+            
             time.sleep(0.1)
-    except KeyboardInterrupt:
-            print("Exiting")
-    finally:
-            set_color(0, 0, 0)
-            GPIO.cleanup()
-if __name__ == "__main__":
-    main()
+
     
     
     
